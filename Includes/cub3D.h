d0a6e41d58f0e3f545b1c231a07cfef0f0407d9d
Hushy <99977761+Hushhhy@users.noreply.github.com>
2024-11-27 15:43:59 +0100

    Add files via upload

---
+++ Includes/cub3D.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarpent <acarpent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 14:14:51 by acarpent          #+#    #+#             */
+/*   Updated: 2024/11/27 14:27:13 by acarpent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+/*---------------------------INCLUDES-------------------------------*/
+
+# include "../lib/libft.h"
+# include "../mlx/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+# include <math.h>
+
+
+/*----------------------Structs and lists---------------------------*/
+
+
+/*------------------------MAIN FUNCTIONS----------------------------*/
+
+#endif
